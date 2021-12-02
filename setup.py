ed2c03acfb1faf7fa7152f2f4f78bee194ee443e
blylei <blylei.info@gmail.com>
2021-12-02 13:43:24 +0800

    init project

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-# (c) 2020 Frabit Project maintained and limited by Blylei < blylei.info@gmail.com >
+# (c) 2020 Dredis Project maintained and limited by Blylei < blylei.info@gmail.com >
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
-# This file is part of Frabit
+# This file is part of frabit-server
 #
 """Backup and Recovery Manager for MySQL
 
