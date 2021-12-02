9f56faf3851996e1101aea9bebcf9c895df27883
blylei <blylei.info@gmail.com>
2021-12-02 14:06:32 +0800

    init project

--- lib/version.py
+++ lib/version.py
@@ -2,3 +2,8 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
 # This file is part of frabit-server
+"""
+This module contains the current frabit-server version.
+"""
+
+__version__ = '2.0.2'
