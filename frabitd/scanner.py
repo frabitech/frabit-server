efdad7d7f06d21a02cb23d18181275569f11405b
blylei <blylei.info@gmail.com>
2021-12-13 15:20:19 +0800

    init project

---
+++ frabitd/scanner.py
@@ -0,0 +1,27 @@
+# (c) 2020 frabit-server Project maintained and limited by FrabiTech < blylei.info@gmail.com >
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
+# This file is part of frabit-server
+"""
+后台备份任务扫描器
+"""
+
+import os
+import re
+import logging
+
+import frabitd
+
+
+_logger = logging.getLogger(__name__)
+
+
+class Scanner():
+    """
+    doc
+    """
+    def __init__(self):
+        pass
+
+    def process(self):
+        pass
